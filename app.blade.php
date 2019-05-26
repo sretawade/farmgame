d6013cba1f663a193acdd6923edc8873eb64273d
sretawade <40015733+sretawade@users.noreply.github.com>
2019-05-26 23:32:17 +0530

    Update app.blade.php

--- app.blade.php
+++ app.blade.php
@@ -1,4 +1,4 @@
-<!DCTYPE html>
+<!DOCTYPE html>
 <html data-whatinput="initial" data-whatintent="mouse" class=" whatinput-types-initial">
   <head>
     <meta http-equiv="content-type" content="text/html; charset=UTF-8">
