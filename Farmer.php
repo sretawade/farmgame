7f7497c48caefebf615eb9751e27a130f3ae7e71
sretawade <40015733+sretawade@users.noreply.github.com>
2019-05-27 08:38:45 +0530

    Update Farmer.php

--- Farmer.php
+++ Farmer.php
@@ -4,7 +4,7 @@ namespace App;
 
 class Farmer extends Mammals
 {
-  protected $m_intFeedingInterval = 2;
+  protected $m_intFeedingInterval = 15;
   protected $m_intMammalCount = 1;
   protected $m_strMammalName = 'Farmer';
 }
